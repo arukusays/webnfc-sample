127078b37ca7e611a4877b4be1a2318e6822fee7
arukusays <syasukura@gmail.com>
2023-10-24 23:20:48 +0900

    Update app.js
    Web NFC APIによる読み込み（スキャン）処理を追加。

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 console.log('app.js');
-document.addEventListener('DOMContentLoaded', e => {
+document.addEventListener('DOMContentLoaded', () => {
+  
   var btn_read = document.getElementById('btn_read');
   var div_msg = document.getElementById('message');
-  btn_read.addEventListener('click', e => {
-    div_msg.innerText = 'Hello, at ' + new Date().toISOString();
+  btn_read.addEventListener('click', async () => {
+    div_msg.innerText = 'clicked read button.';
+    // NFC読み込みイベント登録
+    try {    
+      const ndef = new NDEFReader();
+      await ndef.scan();
+      div_msg.innerText = 'scan started';
+  
+      ndef.addEventListener('error', e => {
+        div_msg.innerText = 'error:' + e;
+      });
+      
+      ndef.addEventListener('reading', e => {
+        div_msg.innerText = `message=${e.message}, serialNumber=${e.serialNumber}`;
+        for(const record of e.message.records){
+          if(record.recordType === 'text'){
+            const decorder = new TextDecoder(record.encoding);
+            const data = decoder.decode(record.data);
+            div_msg.innerText = data;
+          }
+        }
+      });
+      
+    } catch (error) {
+      div_msg.innerText = 'try:error:' + error;
+    }
+
   });
 });
